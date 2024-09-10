c46178fe1b39059fccce940736ae9164973bd436
sherif altiby <sherifahmed852002@gmail.com>
2024-09-10 20:40:07 +0300

    'uodatecv

--- src/components/responsivemenu/Responsivemenue.tsx
+++ src/components/responsivemenu/Responsivemenue.tsx
@@ -13,7 +13,7 @@ const Responsivemenue: React.FC<Props> = ({setControleMenu}) => {
     <div className="bg-stone-700 h-screen text-white w-[250px] text-center flex items-center justify-center" >
         <div>
              {links.map((item) => <div key = {item.id} className="my-10" onClick={() => setControleMenu(false)} > <a  className="no-underline text-white text-3xl" href={item.link}> {item.name} </a> </div>)}
-             <div> <a href="https://drive.google.com/file/d/1_K0Afc4c25c0F9QlTkxdqdcrtuuOGy59/view?usp=sharing" className="bg-btnColor no-underline cursor-pointer text-light py-3 px-5 text-xl rounded-md hover:bg-[#126ffa]" > Resume</a> </div>
+             <div> <a href="https://drive.google.com/file/d/1CjE2qJx20UVd-ZDu4AQWlfa2X2kAvT-w/view?usp=sharing" className="bg-btnColor no-underline cursor-pointer text-light py-3 px-5 text-xl rounded-md hover:bg-[#126ffa]" > Resume</a> </div>
         </div>
     </div>
   )
